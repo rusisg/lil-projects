808bee02f5a33ee397d3eaa87ecbdb996bcc9ceb
rusisg <rasul.nulrybekov@gmail.com>
2024-12-03 21:14:31 +0100

    corrected

--- laundry-auto-registr/script.go
+++ laundry-auto-registr/script.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"github.com/joho/godotenv"
-	"io"
-	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"time"
 )
@@ -17,69 +15,38 @@ func main() {
 		fmt.Println("Error loading .env file")
 	}
 
-	url := os.Getenv("URL")                         // The URL to check
-	buttonPressURL := os.Getenv("BUTTON_PRESS_URL") // The API endpoint for pressing the button
+	urlToCheck := os.Getenv("URL")                  // The URL to check
+	buttonPressURL := os.Getenv("BUTTON_PRESS_URL") // The form submission endpoint
 
 	timeZone := time.FixedZone("GMT+1", 1*60*60)
-	targetTime := time.Date(2024, time.Now().Month(), time.Now().Day(), 18, 16, 0, 0, timeZone)
+	targetTime := time.Date(2024, time.Now().Month(), time.Now().Day(), 17, 16, 0, 0, timeZone)
+	//targetTime := time.Date(2024, time.Now().Month(), time.Now().Day(), time.Now().Hour(), time.Now().Minute(), 0, 0, timeZone)
 
 	for {
 		now := time.Now()
-		log.Println("Starting the loop...")
 
 		if now.After(targetTime) {
 			// Check the status of the URL
-			resp, err := http.Get(url)
-			fmt.Println("Get URL")
+			resp, err := http.Get(urlToCheck)
 			if err != nil {
 				fmt.Printf("Error: %v\n", err)
 				return
 			}
 
-			defer func(Body io.ReadCloser) {
-				err := Body.Close()
-				if err != nil {
-					log.Fatal(err)
-				}
-				fmt.Println("Body close")
-			}(resp.Body)
+			defer resp.Body.Close()
 
-			body, err := io.ReadAll(resp.Body)
-			if err != nil {
-				fmt.Printf("Error while reading -> %v\n", err)
-				return
-			}
-			fmt.Printf("Received: %s\n", body)
+			// Submit the form to press the button
+			formData := url.Values{}
+			formData.Set("reg", "20241125-08:00-8") // Replace with the actual value from your HTML
 
-			// Register the laundry automatically (press the button)
-			requestBody := []byte(`{"action": "press"}`) // Replace with the actual request body if needed
-
-			req, err := http.NewRequest("POST", buttonPressURL, bytes.NewBuffer(requestBody))
-			if err != nil {
-				fmt.Printf("Error creating request: %v\n", err)
-				return
-			}
-
-			client := &http.Client{}
-			resp, err = client.Do(req)
+			resp, err = http.PostForm(buttonPressURL, formData)
 			if err != nil {
 				fmt.Printf("Error sending request: %v\n", err)
 				return
 			}
+			defer resp.Body.Close()
 
-			defer func(Body io.ReadCloser) {
-				err := Body.Close()
-				if err != nil {
-					log.Fatal(err)
-				}
-			}(resp.Body)
-
-			responseBody, err := io.ReadAll(resp.Body)
-			if err != nil {
-				fmt.Printf("Error reading response: %v\n", err)
-				return
-			}
-			fmt.Printf("Button press response: %s\n", responseBody)
+			fmt.Printf("Button press response status: %s\n", resp.Status)
 
 			break
 		}
