560e132a9764c88ce05c09aca0dbf1e7034b7f84
rusisg <rasul.nulrybekov@gmail.com>
2024-12-08 17:59:50 +0100

    eventually code is not working but still on process

--- laundry-auto-registr/script.go
+++ laundry-auto-registr/script.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"context"
 	"fmt"
+	"github.com/joho/godotenv"
 	"net/http"
 	"net/url"
 	"os"
 	"time"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
@@ -17,51 +15,47 @@ func main() {
 		fmt.Println("Error loading .env file")
 	}
 
-	urlToCheck := os.Getenv("URL")                  // The URL to check
-	buttonPressURL := os.Getenv("BUTTON_PRESS_URL") // The form submission endpoint
+	urlToCheck := os.Getenv("URL")
+	buttonPressURL := os.Getenv("BUTTON_PRESS_URL")
 
+	// Define the target time range
 	timeZone := time.FixedZone("GMT+1", 1*60*60)
-	targetTime := time.Date(2024, time.Now().Month(), time.Now().Day(), 17, 16, 0, 0, timeZone)
-	// targetTime := time.Date(2024, time.Now().Month(), time.Now().Day(), time.Now().Hour(), time.Now().Minute(), 0, 0, timeZone)
+	timeStart := time.Date(2024, time.Now().Month(), time.Now().Day(), 17, 47, 0, 0, timeZone)
+	timeStop := time.Date(2024, time.Now().Month(), time.Now().Day(), 17, 50, 0, 0, timeZone)
 
-	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(30*time.Second))
-	defer cancel()
-
-	for {
-		now := time.Now()
-
-		select {
-		case <-ctx.Done():
-			fmt.Println("Context deadline exceeded. Exiting loop...")
-			return
-		default:
-		}
-		if now.After(targetTime) {
-			// Check the status of the URL
-			resp, err := http.Get(urlToCheck)
-			if err != nil {
-				fmt.Printf("Error: %v\n", err)
-				return
-			}
-
-			defer resp.Body.Close()
-
-			// Submit the form to press the button
-			formData := url.Values{}
-			formData.Set("reg", "20241125-08:00-8") // Replace with the actual value from your HTML
-
-			resp, err = http.PostForm(buttonPressURL, formData)
-			if err != nil {
-				fmt.Printf("Error sending request: %v\n", err)
-				return
-			}
-			defer resp.Body.Close()
-
-			fmt.Printf("Button press response status: %s\n", resp.Status)
-
-			break
-		}
-
-		time.Sleep(10 * time.Second)
+	// Wait until timeStart
+	for time.Now().Before(timeStart) {
+		time.Sleep(1 * time.Second)
+		fmt.Printf("Waiting for the start time: %s...\n", timeStart)
 	}
+
+	// Execute the loop between timeStart and timeStop
+	for time.Now().Before(timeStop) {
+		// Perform the GET request
+		resp, err := http.Get(urlToCheck)
+		if err != nil {
+			fmt.Printf("Error performing GET request: %v\n", err)
+			return
+		}
+		resp.Body.Close() // Close response body to avoid resource leaks
+
+		// Perform the POST request
+		formData := url.Values{}
+		formData.Set("reg", "20241125-08:00-8")
+
+		resp, err = http.PostForm(buttonPressURL, formData)
+		if err != nil {
+			fmt.Printf("Error sending POST request: %v\n", err)
+			return
+		}
+		defer resp.Body.Close()
+
+		fmt.Printf("Button press response status: %s\n", resp.Status)
+
+		// Exit after successfully pressing the button
+		break
+	}
+
+	// If the script reaches here, the operation is complete or timeStop has been exceeded
+	fmt.Println("Operation completed or time range expired.")
 }
